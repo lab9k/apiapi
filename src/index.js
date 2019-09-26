257a07e5c29032289de22867fefe43c514034901
Jefwillems <jefwillems100@gmail.com>
2019-09-26 15:10:05 +0200

    add api route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import Koa from 'koa';
+import Router from 'koa-router';
+import serve from 'koa-static';
+import path from 'path';
+import { flatten } from 'lodash/fp';
 import testApis from './test/test_apis';
-// import Router from 'koa-router';
+
+const appDir = path.dirname(require.main.filename);
 
 const app = new Koa();
-app.use(async (ctx) => {
+const router = new Router();
+
+app.use(serve(`${appDir}/public`));
+
+router.get('/api', async (ctx) => {
   const data = await Promise.all(testApis.map((api) => api.invoke()));
-  ctx.body = data;
+  ctx.body = flatten(data);
 });
+
+router.all('*', async (ctx) => {
+  ctx.redirect('/');
+});
+
+app.use(router.routes())
+  .use(router.allowedMethods());
 
 export default app;
