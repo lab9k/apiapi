c1dca872ea50734d9e12cb1b1f171e238c945686
bart <bart.delrue@digipolis.gent>
2019-11-27 16:16:34 +0100

    Simplified i18n for using a single language.

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -3,22 +3,13 @@ import VueI18n from 'vue-i18n'
 
 Vue.use(VueI18n)
 
-export default ({ app, store }) => {
+export default ({ app }) => {
   // Set i18n instance on app
   // This way we can use it in middleware and pages asyncData/fetch
   app.i18n = new VueI18n({
-    locale: store.state.locale,
-    fallbackLocale: 'en',
+    locale: 'en',
     messages: {
       en: require('~/locales/en.json')
     }
   })
-
-  app.i18n.path = (link) => {
-    if (app.i18n.locale === app.i18n.fallbackLocale) {
-      return `/${link}`
-    }
-
-    return `/${app.i18n.locale}/${link}`
-  }
 }
