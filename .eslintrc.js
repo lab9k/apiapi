cc303d319f4433d91f17957bd7ed0ba7daeac684
Jefwillems <jefwillems100@gmail.com>
2019-11-28 13:54:27 +0100

    allow debugger statements in development mode

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   // add your custom rules here
   rules: {
     'nuxt/no-cjs-in-config': 'off',
-    'no-console': ["error", { allow: ["warn", "error"] }]
+    'no-console': ["error", { allow: ["warn", "error"] }],
+    'no-debugger': process.env.NODE_ENV === 'production' ? "error": "off"
   }
 };
