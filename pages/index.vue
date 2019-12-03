3f43e553f7e6603f84ca4b45cec3b57a8860a511
Jefwillems <jefwillems100@gmail.com>
2019-12-03 11:23:41 +0100

    fix link

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-btn :to="'api'" v-text="$tc('api')" />
+    <v-btn :to="'apis'" v-text="$tc('api')" />
     <v-btn @click="invokeAll" v-text="$t('invoke_all')" class="ml-5" />
     <j-s-o-n-view :data="allData" v-if="jsonVisible" />
   </v-container>
