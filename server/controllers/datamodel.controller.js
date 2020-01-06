e6474ca72426cd28c3388c69ad487e9c75fe3200
Jefwillems <jefwillems100@gmail.com>
2020-01-06 10:05:21 +0100

    add datamodel controller

--- server/controllers/datamodel.controller.js
+++ server/controllers/datamodel.controller.js
@@ -1,6 +1,11 @@
+const DataModelModel = require('../db/models/datamodel.db.model')
 
 module.exports = {
-  findAll (req, res) {
-    return res.json({})
+  findAll (req, res, next) {
+    return DataModelModel.getAll().exec().then(result => res.json(result)).catch(next)
+  },
+  create (req, res, next) {
+    const newDataModel = new DataModelModel(req.body)
+    DataModelModel.addDataModel(newDataModel).then(doc => res.json(doc)).catch(next)
   }
 }
