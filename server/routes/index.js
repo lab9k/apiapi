0129c90131147a9c1bfec6f6d65d356c84692cf8
bart <bart.delrue@digipolis.gent>
2020-01-23 14:32:59 +0100

    Added backend proxy to avoid CORS.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const HttpService = require('../services/http.service')
 const apiRoutes = require('./api.routes')
 const dataRoutes = require('./data.routes')
 const serverRoutes = require('./server.routes')
@@ -10,5 +11,19 @@ router.use('/api', apiRoutes)
 router.use('/data', dataRoutes)
 router.use('/server', serverRoutes)
 router.use('/collection', collectionRoutes)
+
+/**
+ * Used to circumvent CORS issues.
+ * To be used behind authentication.
+ */
+router.get('/proxy/:url', (req, res, next) => {
+  const client = new HttpService(Buffer.from(req.params.url, 'base64').toString())
+  client.get()
+    .then(({ data }) => res.json(data))
+    .catch(({ response }) => {
+      res.status(response.status)
+      res.json(response.statusText)
+    })
+})
 
 module.exports = router
